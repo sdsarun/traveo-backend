55c6b5d49d65814c50e88c72a73f930a0b53a25e
Sarun Daunghirun <sdsarun@outlook.com>
2024-12-03 18:21:58 +0700

    feat: setup swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigurationsModule } from './configurations/configurations.module';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './services/users/users.module';
 
 @Module({
   imports: [
     ConfigurationsModule,
+    DatabaseModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,18 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors();
+
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: "1" });
+
+  // swagger
+  const swaggerDocumentBuilder = new DocumentBuilder();
+  swaggerDocumentBuilder.setTitle('Traveo')
+  swaggerDocumentBuilder.setDescription("Trip planner API")
+  swaggerDocumentBuilder.setVersion('1.0')
+
+  const swaggerDocumentBuild = swaggerDocumentBuilder.build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerDocumentBuild);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
