7d93842bd347bc826fc17985bdbbb24b71b1ad5b
Sarun Daunghirun <sdsarun@outlook.com>
2024-12-03 17:43:11 +0700

    fix: config helmet, cookie parser, cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(cookieParser());
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
