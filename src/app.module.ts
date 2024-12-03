e389485c0e2bc715b83a2a4d70e04fb5a6aedb2a
Sarun Daunghirun <sdsarun@outlook.com>
2024-12-03 18:42:13 +0700

    feat: init health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ConfigurationsModule } from './configurations/configurations.module';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './services/users/users.module';
+import { HealthModule } from './services/health/health.module';
 
 @Module({
   imports: [
     ConfigurationsModule,
     DatabaseModule,
+    HealthModule,
     UsersModule,
   ],
   controllers: [AppController],
